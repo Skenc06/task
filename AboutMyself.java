4ba5d86cc7639cf95cc2526b668424bc4d823c34
Serkan Kenc <71468761+Skenc06@users.noreply.github.com>
2020-09-19 22:57:00 +0200

    Add files via upload

---
+++ AboutMyself.java
@@ -0,0 +1,19 @@
+package a__review;
+
+public class AboutMyself {
+
+	public static void main(String[] args) {
+		
+		String name = "Serkan";
+		int age = 43;
+		String city = "Chicago";
+		int houseHoldNnumber = 5;
+		
+		System.out.println("My name is " + name + "," + " I am " + age + " years old" + "," + " I live in " + city + "," + " I have " + houseHoldNnumber + " family member");
+		System.out.println("I am already loving java !!!");
+		
+		
+
+	}
+
+}
